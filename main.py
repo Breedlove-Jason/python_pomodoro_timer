e55d7345773955553f2648f03f8ef6d09c85edce
Jason Breedlove <jbreedlove@jasonbreedlove.dev>
2023-05-08 13:04:42 -0700

    changed to grid

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
 timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
-timer_label.pack()
+timer_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
 
 checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 20))
-checkmark_label.pack()
+checkmark_label.grid(column=1, row=4)
 
 start_button = Button(text="Start", highlightthickness=0)
-start_button.pack(side="left")
+start_button.grid(column=0, row=3)
 
 reset_button = Button(text="Reset", highlightthickness=0)
-reset_button.pack(side="right")
+reset_button.grid(column=2, row=3)
 
 window.mainloop()
